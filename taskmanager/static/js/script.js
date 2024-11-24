a2a96f3deda7465b9bec00bb4ec6dbf9169d8d6a
Oliver Griffin <olivergriffin2991@gmail.com>
2024-11-24 16:14:56 +0000

    created func for adding new tasks to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     MIDIAccess.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
   });
+
