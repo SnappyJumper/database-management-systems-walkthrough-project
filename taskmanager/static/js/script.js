b68c9a996fae9c660b32064f1dcd9d27e5b3cd4c
Oliver Griffin <olivergriffin2991@gmail.com>
2024-11-24 12:46:23 +0000

    created css and java files and linked them to the base template, finished some basic features of the base template and then linked the base template to my new home screen Tasks.

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    MIDIAccess.Sidenav.init(sidenav);
+  });
